d01c4efb6c68317920c394705fa1c36b03d4d93b
Wy979565895 <65142776+Wy979565895@users.noreply.github.com>
2025-03-23 22:49:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["docker."docker.cqsyjq.cn]: dockerHub,
+  ["quay."docker.cqsyjq.cn]: "https://quay.io",
+  ["gcr."docker.cqsyjq.cn]: "https://gcr.io",
+  ["k8s-gcr."docker.cqsyjq.cn]: "https://k8s.gcr.io",
+  ["k8s."docker.cqsyjq.cn]: "https://registry.k8s.io",
+  ["ghcr."docker.cqsyjq.cn]: "https://ghcr.io",
+  ["cloudsmith."docker.cqsyjq.cn]: "https://docker.cloudsmith.io",
   ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
